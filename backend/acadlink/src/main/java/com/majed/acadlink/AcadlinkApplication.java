328c2202ceef823ecd1cc58ec48eafe118341fb9
mr_majed7 <majedulislam7789@gmail.com>
2025-01-23 01:39:49 +0600

    Initialize backend structure with Spring Boot, add application configuration, and set up testing framework

---
+++ backend/acadlink/src/main/java/com/majed/acadlink/AcadlinkApplication.java
@@ -0,0 +1,13 @@
+package com.majed.acadlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcadlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcadlinkApplication.class, args);
+	}
+
+}
---
+++ backend/acadlink/src/test/java/com/majed/acadlink/AcadlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majed.acadlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcadlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
