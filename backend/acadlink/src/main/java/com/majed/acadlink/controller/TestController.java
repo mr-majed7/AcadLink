870e275a3299c89113f456e734d05c886750c4c5
mr_majed7 <majedulislam7789@gmail.com>
2025-01-23 03:40:47 +0600

    ``` chore: update TestController with API documentation (main) ```

--- backend/acadlink/src/main/java/com/majed/acadlink/controller/TestController.java
+++ backend/acadlink/src/main/java/com/majed/acadlink/controller/TestController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    /**
+     * This is a test api controller
+     *
+     * @return String
+     */
     @GetMapping("/hello")
     public String test() {
         return "Hello, This is test controller";
