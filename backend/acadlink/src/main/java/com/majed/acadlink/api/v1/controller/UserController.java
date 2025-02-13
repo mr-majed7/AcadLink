3741f2e6e5a3ad29a0b7d24bac90bca021077826
mr_majed7 <majedulislam7789@gmail.com>
2025-02-13 20:33:35 +0600

    feat: add UserController with endpoint to retrieve authenticated user details

---
+++ backend/acadlink/src/main/java/com/majed/acadlink/api/v1/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.majed.acadlink.api.v1.controller;
+
+import com.majed.acadlink.domain.entitie.User;
+import com.majed.acadlink.dto.user.UserResponseDTO;
+import com.majed.acadlink.service.UserService;
+import com.majed.acadlink.utility.GetUserUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private GetUserUtil getUserUtil;
+
+    @GetMapping("get-user")
+    public ResponseEntity<UserResponseDTO> getUser() {
+        User user = getUserUtil.getAuthenticatedUser().get();
+
+        UserResponseDTO response = new UserResponseDTO(user.getId(),
+                user.getFirstName(), user.getLastName(), user.getInstitute(), user.getEmail(),
+                user.getUsername(), user.getCreatedAt()
+        );
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+}
