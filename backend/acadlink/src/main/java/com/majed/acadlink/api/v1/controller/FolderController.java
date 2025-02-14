03e2dac095b3b2afd1246f186b695dceb482119b
mr_majed7 <majedulislam7789@gmail.com>
2025-02-15 04:11:05 +0600

    refactor: simplify FolderController and FolderService by removing unused dependencies and streamlining response handling

--- backend/acadlink/src/main/java/com/majed/acadlink/api/v1/controller/FolderController.java
+++ backend/acadlink/src/main/java/com/majed/acadlink/api/v1/controller/FolderController.java
@@ -1,22 +1,17 @@
 package com.majed.acadlink.api.v1.controller;
 
-import com.majed.acadlink.domain.entitie.Folder;
-import com.majed.acadlink.domain.repository.FolderRepo;
 import com.majed.acadlink.dto.folder.AllFolderResponseDTO;
 import com.majed.acadlink.dto.folder.FolderCreateDTO;
 import com.majed.acadlink.dto.folder.FolderResponseDTO;
 import com.majed.acadlink.dto.folder.UpdateFolderResponseDTO;
 import com.majed.acadlink.service.FolderService;
-import com.majed.acadlink.utility.AuthorizationCheck;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Tag(name = "3. Folder Management", description = "Endpoints for managing folders")
@@ -25,72 +20,34 @@ import java.util.UUID;
 @Slf4j
 public class FolderController {
     private final FolderService folderService;
-    private final AuthorizationCheck authorizationCheck;
-    private final FolderRepo folderRepo;
 
-    public FolderController(FolderService folderService, AuthorizationCheck authorizationCheck,
-                            FolderRepo folderRepo) {
+    public FolderController(FolderService folderService) {
         this.folderService = folderService;
-        this.authorizationCheck = authorizationCheck;
-        this.folderRepo = folderRepo;
     }
 
     @Operation(summary = "Create a new folder", tags = {"3. Folder Management"})
     @PostMapping("/create")
     public ResponseEntity<AllFolderResponseDTO> createFolder(@RequestBody FolderCreateDTO folderData) {
-        try {
-            AllFolderResponseDTO addedFolder = folderService.addFolder(folderData);
-            return new ResponseEntity<>(addedFolder, HttpStatus.CREATED);
-        } catch (Exception e) {
-            log.error(e.toString());
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+        return folderService.addFolder(folderData);
     }
+
 
     @Operation(summary = "Get all folders", tags = {"3. Folder Management"})
     @GetMapping("/get-all")
     public ResponseEntity<List<AllFolderResponseDTO>> getAllFolders() {
-        List<AllFolderResponseDTO> folders = folderService.getAllFolders();
-
-        if (folders.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        } else {
-            return new ResponseEntity<>(folders, HttpStatus.OK);
-        }
+        return folderService.getAllFolders();
     }
 
     @Operation(summary = "Get a specific folder by ID", tags = {"3. Folder Management"})
     @GetMapping("/get-folder/{folderId}")
     public ResponseEntity<FolderResponseDTO> getFolder(@PathVariable UUID folderId) {
-        Optional<Folder> folder = folderRepo.findById(folderId);
-
-        if (folder.isPresent()) {
-            if (authorizationCheck.checkAuthorization(folder.get().getUser().getId())) {
-                FolderResponseDTO response = folderService.getFolder(folder.get());
-                return new ResponseEntity<>(response, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-            }
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        return folderService.getFolder(folderId);
     }
 
     @Operation(summary = "Update a specific folder by ID", tags = {"3. Folder Management"})
     @PutMapping("/update-folder/{folderId}")
     public ResponseEntity<UpdateFolderResponseDTO> updateFolder(@PathVariable UUID folderId, @RequestBody FolderCreateDTO newData) {
-        Optional<Folder> folder = folderRepo.findById(folderId);
-
-        if (folder.isPresent()) {
-            if (authorizationCheck.checkAuthorization(folder.get().getUser().getId())) {
-                UpdateFolderResponseDTO response = folderService.updateFolder(folder.get(), newData);
-                return new ResponseEntity<>(response, HttpStatus.CREATED);
-            } else {
-                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-            }
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        return folderService.updateFolder(folderId, newData);
     }
     //WORK ON DELETE FOLDER AFTER FINISHING MATERIAL UPDATE AND DELETE
 
--- backend/acadlink/src/main/java/com/majed/acadlink/service/FolderService.java
+++ backend/acadlink/src/main/java/com/majed/acadlink/service/FolderService.java
@@ -8,27 +8,33 @@ import com.majed.acadlink.dto.folder.FolderCreateDTO;
 import com.majed.acadlink.dto.folder.FolderResponseDTO;
 import com.majed.acadlink.dto.folder.UpdateFolderResponseDTO;
 import com.majed.acadlink.dto.material.MaterialResponseDTO;
+import com.majed.acadlink.utility.AuthorizationCheck;
 import com.majed.acadlink.utility.GetUserUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @Slf4j
 public class FolderService {
     private final FolderRepo folderRepo;
     private final GetUserUtil getUserUtil;
+    private final AuthorizationCheck authorizationCheck;
 
-    public FolderService(FolderRepo folderRepo, GetUserUtil getUserUtil) {
+    public FolderService(FolderRepo folderRepo, GetUserUtil getUserUtil, AuthorizationCheck authorizationCheck) {
         this.folderRepo = folderRepo;
         this.getUserUtil = getUserUtil;
+        this.authorizationCheck = authorizationCheck;
     }
 
 
-    public AllFolderResponseDTO addFolder(FolderCreateDTO folderData) {
+    public ResponseEntity<AllFolderResponseDTO> addFolder(FolderCreateDTO folderData) {
         Optional<User> user = getUserUtil.getAuthenticatedUser();
         if (user.isPresent()) {
             Folder folder = new Folder();
@@ -36,38 +42,72 @@ public class FolderService {
             folder.setPrivacy(folderData.getPrivacy());
             folder.setUser(user.get());
             Optional<Folder> addedFolder = Optional.of(folderRepo.save(folder));
-            return new AllFolderResponseDTO(addedFolder.get().getId(), addedFolder.get().getName(),
+            AllFolderResponseDTO response = new AllFolderResponseDTO(addedFolder.get().getId(), addedFolder.get().getName(),
                     addedFolder.get().getCreatedAt(), addedFolder.get().getPrivacy());
+            return new ResponseEntity<>(response, HttpStatus.OK);
         } else {
             log.error("User does not exists");
-            return null;
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
 
-    public List<AllFolderResponseDTO> getAllFolders() {
+    public ResponseEntity<List<AllFolderResponseDTO>> getAllFolders() {
         Optional<User> user = getUserUtil.getAuthenticatedUser();
 
         if (user.isPresent()) {
             List<Folder> folders = folderRepo.findByUserId(user.get().getId());
-            return folders.stream()
+            List<AllFolderResponseDTO> folderList = folders.stream()
                     .map(folder -> new AllFolderResponseDTO(folder.getId(), folder.getName(),
                             folder.getCreatedAt(), folder.getPrivacy()))
                     .toList();
+            return new ResponseEntity<>(folderList, HttpStatus.OK);
         } else {
-            return Collections.EMPTY_LIST;
+            return new ResponseEntity<>(Collections.emptyList(), HttpStatus.BAD_REQUEST);
         }
     }
 
-    public FolderResponseDTO getFolder(Folder folder) {
-        List<MaterialResponseDTO> materials = folder.getMaterials().stream()
-                .map(material -> new MaterialResponseDTO(material.getId(), material.getName(), material.getLink(),
-                        material.getType(), material.getPrivacy(), folder.getId()))
-                .toList();
+    public ResponseEntity<FolderResponseDTO> getFolder(UUID folderId) {
+        Optional<User> user = getUserUtil.getAuthenticatedUser();
+        if (user.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } else {
+            if (!authorizationCheck.checkAuthorization(user.get().getId())) {
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+            } else {
+                Optional<Folder> findFolder = folderRepo.findById(folderId);
+                if (findFolder.isPresent()) {
+                    Folder folder = findFolder.get();
+                    List<MaterialResponseDTO> materials = folder.getMaterials().stream()
+                            .map(material -> new MaterialResponseDTO(material.getId(), material.getName(), material.getLink(),
+                                    material.getType(), material.getPrivacy(), folder.getId()))
+                            .toList();
 
-        return new FolderResponseDTO(folder.getId(), folder.getName(), folder.getCreatedAt(), folder.getPrivacy(), materials);
+                    FolderResponseDTO folderResponse = new FolderResponseDTO(folder.getId(), folder.getName(),
+                            folder.getCreatedAt(), folder.getPrivacy(), materials);
+                    return new ResponseEntity<>(folderResponse, HttpStatus.OK);
+                } else {
+                    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+                }
+            }
+        }
     }
 
-    public UpdateFolderResponseDTO updateFolder(Folder folder, FolderCreateDTO newData) {
+    public ResponseEntity<UpdateFolderResponseDTO> updateFolder(UUID folderId, FolderCreateDTO newData) {
+        Optional<User> user = getUserUtil.getAuthenticatedUser();
+        if (user.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (!authorizationCheck.checkAuthorization(user.get().getId())) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+
+        return folderRepo.findById(folderId)
+                .map(folder -> updateFolderData(folder, newData))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    private ResponseEntity<UpdateFolderResponseDTO> updateFolderData(Folder folder, FolderCreateDTO newData) {
         if (newData.getName() != null) {
             folder.setName(newData.getName());
         }
@@ -76,9 +116,8 @@ public class FolderService {
         }
 
         Folder updatedFolder = folderRepo.save(folder);
-
-        return new UpdateFolderResponseDTO(updatedFolder.getId(), updatedFolder.getName()
-                , updatedFolder.getCreatedAt(), updatedFolder.getPrivacy()
-        );
+        UpdateFolderResponseDTO response = new UpdateFolderResponseDTO(updatedFolder.getId(),
+                updatedFolder.getName(), updatedFolder.getCreatedAt(), updatedFolder.getPrivacy());
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 }
